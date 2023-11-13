142364986b2f84e326cbe718df0ecd7e00ac785a
Adinath Shelke <shelkeadinath3@gmail.com>
2023-11-13 20:28:57 +0530

    Move zeros (283)

--- 283.cpp
+++ 283.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                for(j=i+1;j<nums.size();j++){
-                    if(nums[j]!=0){
-                        swap(nums[i],nums[j]);
-                        break;
-                    }
-                }
-                if(j==nums.size())
-                    return;
+        int i=0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;
             }
         }
     }
